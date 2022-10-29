087c85ea4f092a61f1e845b7eeca09d02c5e27b4
saaasuuukaae <philipchef13@gmail.com>
2022-10-29 17:11:58 +0300

    [ADD] - Base SEO optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,23 @@ import {
     DisplayImages
 } from '@/src/components/layout/DisplayImages/DisplayImages';
 import type {NextPage} from 'next'
+import {SEO} from "@/components/layout/SEO/SEO";
 
 
 const Images: NextPage = () => {
 
     return (
-        <DisplayImages/>
+        <>
+            <SEO
+                title={"Hentai World"}
+                description={"Hentai World is a website where you can find the best hentai images and videos."}
+                keywords={"hentai, hentai world, hentai images, hentai videos, hentai gifs, hentai world images, hentai world videos, hentai world gifs"}
+
+
+            />
+
+            <DisplayImages/>
+        </>
     )
 
 }
--- src/components/layout/SEO/SEO.tsx
+++ src/components/layout/SEO/SEO.tsx
@@ -1,7 +1,42 @@
 import Head from "next/head";
+import logo from "@/public/assets/logo.png";
 
-export function SEO() {
+interface Interface {
+
+    title: string;
+    description: string;
+    keywords: string;
+
+
+}
+
+export function SEO({title, description, keywords}: Interface) {
     return (
-        <Head><title>Some title</title></Head>
+        <Head>
+
+            <title>{title}</title>
+
+            {/* og meta tags */}
+            <meta property="og:title" content={title}/>
+            <meta property="og:description" content={description}/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:locale" content="en_US"/>
+            <meta property="og:site_name" content="Hentai World"/>
+
+            {/* Image */}
+            <meta property="og:image" content={logo.src}/>
+
+
+            {/* twitter meta tags */}
+            <meta name="twitter:card" content="summary"/>
+            <meta name="twitter:title" content={title}/>
+            <meta name="twitter:description" content={description}/>
+
+            {/* other meta tags */}
+            <meta name="description" content={description}/>
+            <meta name="keywords" content={keywords}/>
+            <meta name={"format-detection"} content={"telephone=no"}/>
+        </Head>
+
     )
 }
