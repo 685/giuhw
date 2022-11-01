6b77cc9a76bb48c885a98596accc186f8ecd788d
saaasuuukaae <philipchef13@gmail.com>
2022-11-01 23:44:57 +0200

    [FIX] - http getting error

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export function getFullUrl(req: IncomingMessage) {
     let fullUrl
     if (req) {
         // Server side rendering
-        fullUrl = req.headers.referer?.split('://')[0] + '://' + req.headers.host + req.url
+        fullUrl = req.headers['x-forwarded-proto'] || req.headers.referer?.split('://')[0] + '://' + req.headers.host + req.url
     } else {
         // Client side rendering
         fullUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
